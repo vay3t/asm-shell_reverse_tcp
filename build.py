82ac2631ab134046325e4e98a0ee0516da45ccee
Vay3t <vay3t@localhost.local>
2025-01-18 03:03:17 -0300

    first

--- build.py
+++ build.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python3
 
-# =============================================================================#
-# A simple python build script to build the singles/stages/stagers and
-# some usefull information such as offsets and a hex dump. The binary output
-# will be placed in the bin directory. A hex string and usefull comments will
-# be printed to screen.
-#
-# Example:
-#     >python build.py stager_reverse_tcp_nx
-#
-# Example, to build everything:
-#     >python build.py all > build_output.txt
-#
-# Author: Stephen Fewer (stephen_fewer[at]harmonysecurity[dot]com)
-# =============================================================================#
-
 import os
 import sys
 import time
@@ -52,15 +37,50 @@ def build(name):
         print("[-] Unable to locate '%s.asm' in the src directory" % name)
 
 
-def xmit_dump_ruby(data, length=16):
+def format_hex(data):
+    return ", ".join(["0x%02x" % x for x in data])
+
+
+def xmit_dump_python(data, length=16):
+    dump = ""
+    for i in range(0, len(data), length):
+        bytes_chunk = data[i : i + length]
+        hex_values = "".join(f"\\x{byte:02X}" for byte in bytes_chunk)
+        dump += f"{hex_values}"
+    print(f'buf = "{dump}"')
+
+
+def xmit_dump_c(data, length=16):
     dump = ""
     for i in range(0, len(data), length):
         bytes = data[i : i + length]
-        hex = '"%s"' % ("".join(["\\x%02X" % x for x in bytes]))
+        hex_values = format_hex(bytes)
         if i + length <= len(data):
-            hex += " +"
-        dump += "%s\n" % (hex)
-    print(dump)
+            hex_values += ","
+        dump += "    %s\n" % (hex_values)
+    print("unsigned char buf[] = {\n" + dump + "};\n")
+
+
+def xmit_dump_csharp(data, length=16):
+    dump = ""
+    for i in range(0, len(data), length):
+        bytes = data[i : i + length]
+        hex_values = format_hex(bytes)
+        if i + length <= len(data):
+            hex_values += ","
+        dump += "        %s\n" % (hex_values)
+    print("static byte[] buf = new byte[]\n    {\n" + dump + "    };")
+
+
+def xmit_dump_powershell(data, length=16):
+    dump = ""
+    for i in range(0, len(data), length):
+        bytes = data[i : i + length]
+        hex_values = format_hex(bytes)
+        if i + length <= len(data):
+            hex_values += ","
+        dump += "    %s\n" % (hex_values)
+    print("$buf = @(" + dump + ")")
 
 
 def xmit_offset(data, name, value):
@@ -69,22 +89,20 @@ def xmit_offset(data, name, value):
         print("# %s Offset: %d" % (name, offset))
 
 
-def xmit(name, dump_ruby=True):
+def xmit(name, dump_c=True):
     bin = os.path.normpath(os.path.join("./bin/", "%s.bin" % name))
-    f = open(bin, "rb")
-    data = f.read()
+    with open(bin, "rb") as f:
+        data = f.read()
     print("# Name: %s\n# Length: %d bytes" % (name, len(data)))
     xmit_offset(data, "Port", pack(">H", 4444))  # 4444
     xmit_offset(data, "Host", pack(">L", 0x7F000001))  # 127.0.0.1
-    # kernel32.dll!ExitThread
     xmit_offset(data, "ExitFunk", pack("<L", 0x0A2A1DE0))
-    # kernel32.dll!ExitProcess
     xmit_offset(data, "ExitFunk", pack("<L", 0x56A2B5F0))
-    # kernel32.dll!SetUnhandledExceptionFilter
     xmit_offset(data, "ExitFunk", pack("<L", 0xEA320EFE))
-    xmit_offset(data, "ExitFunk", pack("<L", 0xE035F044))  # kernel32.dll!Sleep
-    if dump_ruby:
-        xmit_dump_ruby(data)
+    xmit_offset(data, "ExitFunk", pack("<L", 0xE035F044))
+    if dump_c:
+        print()
+        xmit_dump_python(data)
 
 
 def main(argv=None):
